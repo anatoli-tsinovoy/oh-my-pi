Sharp336/grimoire-agent-engine

fix(ai): preserve audio and video media across the session lifecycle
Remote compaction serializes video and tool-result media; queued-prompt restoration preserves media; media externalized on persistence; media-only tool results render; share stripping placeholders media; before_agent_start carries attachments; OTel emits media blob parts; token estimates are bounded and modality-aware; capability routing falls back to declared vendor input; Copilot discovery stays authoritative; preserved effort routes seeded.

packages/coding-agent/src/session/session-loader.ts
@@ -11,7 +11,7 @@ import {
 	type SessionTitleSlotEntry,
 } from "./session-entries";
 import { migrateToCurrentVersion } from "./session-migrations";
-import { isImageBlock, isImageDataPayload } from "./session-persistence";
+import { isMediaBlock, isMediaDataPayload } from "./session-persistence";
 import { FileSessionStorage, type SessionStorage } from "./session-storage";
 import {
 	parseTitleSlotFromContent,
@@ -189,13 +189,13 @@ function hasImageUrl(value: unknown): value is { image_url: string } {
 	return typeof value === "object" && value !== null && "image_url" in value && typeof value.image_url === "string";
 }
 
-function shouldResolveImagePayload(value: unknown, key: string | undefined): value is { data: string } {
-	if (!isImageDataPayload(value) || !isBlobRef(value.data)) return false;
-	return (key === "content" && isImageBlock(value)) || key === "images";
+function shouldResolveMediaPayload(value: unknown, key: string | undefined): value is { data: string } {
+	if (!isMediaDataPayload(value) || !isBlobRef(value.data)) return false;
+	return (key === "content" && isMediaBlock(value)) || key === "images";
 }
 
 async function resolvePersistedBlobRefs(value: unknown, blobStore: BlobStore, key?: string): Promise<void> {
-	if (shouldResolveImagePayload(value, key)) {
+	if (shouldResolveMediaPayload(value, key)) {
 		value.data = await resolveImageData(blobStore, value.data);
 		return;
 	}

anatoli-tsinovoy/oh-my-pi

packages/coding-agent/src/session/session-loader.ts
@@ -5,7 +5,7 @@ import { BlobStore, isBlobRef, resolveImageData, resolveImageDataUrl } from "./b
 import { buildSessionContext } from "./session-context";
 import type { FileEntry, RawFileEntry, SessionEntry, SessionHeader } from "./session-entries";
 import { migrateToCurrentVersion } from "./session-migrations";
-import { isExternalizableImagePosition, isPersistenceTruncatedString } from "./session-persistence";
+import { isExternalizableMediaPosition, isPersistenceTruncatedString } from "./session-persistence";
 import { FileSessionStorage, type SessionStorage } from "./session-storage";
 import {
 	parseTitleSlotFromContent,
@@ -330,9 +330,12 @@ export async function visitEntriesFromFile(
 function hasImageUrl(value: unknown): value is { image_url: string } {
 	return typeof value === "object" && value !== null && "image_url" in value && typeof value.image_url === "string";
 }
+function shouldResolveMediaPayload(value: unknown, key: string | undefined): value is { data: string } {
+	return isExternalizableMediaPosition(value, key) && isBlobRef(value.data);
+}
 
 async function resolvePersistedBlobRefs(value: unknown, blobStore: BlobStore, key?: string): Promise<void> {
-	if (isExternalizableImagePosition(value, key) && isBlobRef(value.data)) {
+	if (shouldResolveMediaPayload(value, key)) {
 		value.data = await resolveImageData(blobStore, value.data);
 		return;
 	}